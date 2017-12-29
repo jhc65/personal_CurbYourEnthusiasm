91a94136b66d1fb3d29c3cb309bdb549587b2a83
Joe <joseph@rkiapps.com>
2017-12-29 15:05:22 -0500

    initial commit

---
+++ CurbYourEnthusiasm/Curb Your Enthousiasm Soundboard/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,28 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    private bool isClipPlaying = false;
+
+    private void Start()
+    {
+        audioSource.clip = audioClip;
+    }
+
+    public void ToggleSound()
+    {
+        if (!isClipPlaying) {
+            isClipPlaying = true;
+            audioSource.Play();
+        }
+        else {
+            isClipPlaying = false;
+            audioSource.Stop();
+        }
+    }
+}
